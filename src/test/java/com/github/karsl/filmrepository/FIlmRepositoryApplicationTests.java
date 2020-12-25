f4ff11d2c232679fc6727d0b907d4c8b7110e535
Kaan Arslan <kaan.arslan@ceng.deu.edu.tr>
2020-12-25 12:17:40 +0300

    Initial commit

---
+++ src/test/java/com/github/karsl/filmrepository/FIlmRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.karsl.filmrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FIlmRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
