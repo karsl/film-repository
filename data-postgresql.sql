8546fbc421962c256ac017103abe6cc372ad0fcd
Kaan Arslan <kaan.arslan@ceng.deu.edu.tr>
2021-01-09 02:21:46 +0300

    Set sequence value after insert

--- data-postgresql.sql
+++ data-postgresql.sql
@@ -1,17 +1,25 @@
+BEGIN TRANSACTION;
+
 INSERT INTO film_genre (id, name)
 VALUES (1, 'Comedy'),
        (2, 'Action'),
        (3, 'Drama'),
        (4, 'Crime');
 
+SELECT setval('film_genre_id_seq', 4);
+
 INSERT INTO media_type (id, name)
 VALUES (1, 'Stream'),
        (2, 'Download');
+
+SELECT setval('media_type_id_seq', 2);
 
 INSERT INTO language (id, name)
 VALUES (1, 'English'),
        (2, 'Italian'),
        (3, 'Latin');
+
+SELECT setval('language_id_seq', 3);
 
 INSERT INTO film (id, title, description, genre, year, media)
 VALUES (1, 'The Shawshank Redemption',
@@ -20,6 +28,8 @@ VALUES (1, 'The Shawshank Redemption',
        (2, 'The Godfather',
         'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
         4, 1972, 2);
+
+SELECT setval('film_id_seq', 2);
 
 INSERT INTO actor (id, full_name)
 VALUES (1, 'Tim Robbins'),
@@ -30,6 +40,8 @@ VALUES (1, 'Tim Robbins'),
        (6, 'Al Pacino'),
        (7, 'James Caan'),
        (8, 'Richard S. Castellano');
+
+SELECT setval('actor_id_seq', 8);
 
 INSERT INTO credit (film_id, actor_id, role)
 VALUES (1, 1, 'Andy Dufresne'),
@@ -47,3 +59,4 @@ VALUES (1, 1),
        (2, 2),
        (2, 3);
 
+COMMIT;
