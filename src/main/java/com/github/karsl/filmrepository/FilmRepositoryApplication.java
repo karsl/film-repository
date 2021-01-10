75e5cdd08552c46460216a9631ac8c07b08f120f
Kaan Arslan <kaan.arslan@ceng.deu.edu.tr>
2021-01-10 17:43:27 +0300

    Fix typo

--- src/main/java/com/github/karsl/filmrepository/FilmRepositoryApplication.java
+++ src/main/java/com/github/karsl/filmrepository/FilmRepositoryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FIlmRepositoryApplication {
+public class FilmRepositoryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FIlmRepositoryApplication.class, args);
+        SpringApplication.run(FilmRepositoryApplication.class, args);
     }
 
 }
--- src/test/java/com/github/karsl/filmrepository/FilmRepositoryApplicationTests.java
+++ src/test/java/com/github/karsl/filmrepository/FilmRepositoryApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FIlmRepositoryApplicationTests {
+class FilmRepositoryApplicationTests {
 
     @Test
     void contextLoads() {
