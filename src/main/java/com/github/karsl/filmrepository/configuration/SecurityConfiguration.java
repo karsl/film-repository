449189f41812ae75ae4323d6d04c1de685cb076c
Kaan Arslan <kaan.arslan@ceng.deu.edu.tr>
2021-01-12 19:15:48 +0300

    Remove unused imports

--- src/main/java/com/github/karsl/filmrepository/configuration/SecurityConfiguration.java
+++ src/main/java/com/github/karsl/filmrepository/configuration/SecurityConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
